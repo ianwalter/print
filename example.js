a82fc17ac6699cca3c3aa7a314953a5c5d1a225c
Ian Walter <public@iankwalter.com>
2019-04-07 13:48:55 -0400

    Fixing #4 and adding #1

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
-const chalk = require('chalk')
-const { print } = require('.')
+const { print, chalk } = require('.')
 
 print.error('Environment variables not set!')
 print.error(new Error('No assertions were executed.'))
 print.error(new Error(chalk.bold('Expected something else.')))
-print.warn('File was overwritten.')
+print.warn('File was overwritten:', '\n', '/tmp/fakeFile.json')
 print.info('Done in 0.91s.')
-print.debug('Flaky test started.')
+print.debug('Flaky test started.', `
+  Make sure you check it out.
+  Could be trouble.
+`)
 print.log('Request made to server.')
-print.success('You did it!')
+print.success('You did it!', 'Great job.')
--- index.js
+++ index.js
@@ -3,18 +3,34 @@ import chalk from 'chalk'
 import hasAnsi from 'has-ansi'
 
 const defaults = {
-  types: ['debug', 'info', 'warn', 'error', 'log', 'success'],
+  types: ['debug', 'info', 'success', 'log', 'warn', 'error'],
   level: 'debug'
 }
-
-const toStackLine = line => {
+const toStackLines = line => {
   if (line.includes('(')) {
     const [ref, path] = line.trim().split(' ')
-    line = `${chalk.bold(ref)} ${chalk.gray(path)}`
-  } else {
-    line = chalk.gray(line.trim())
+    return `${chalk.bold(ref)} ${chalk.gray(path)}`
   }
-  return line
+  return chalk.gray(line.trimStart())
+}
+const toPaddedLines = full => line => `   ${full ? ' ' : ''}${line.trimStart()}`
+const toRest = (item, index, items) => {
+  let [newline, ...rest] = item.split('\n')
+
+  // Handle formatting an item with newlines.
+  if (rest.length && rest[0]) {
+    newline = newline.replace(' ', '') === '' ? '\n' : newline
+    return newline + rest.map(toPaddedLines(true)).join('\n')
+  }
+
+  // Handle formatting an item that comes after a newline.
+  const last = index > 0 && items[index - 1]
+  const lastIsString = typeof last === 'string'
+  if (lastIsString && last.replace(' ', '')[last.length - 1] === '\n') {
+    return toPaddedLines()(item)
+  }
+
+  return item
 }
 
 export class Print {
@@ -27,42 +43,44 @@ export class Print {
     if (err instanceof Error) {
       const [_, ...lines] = err.stack.split('at')
       const at = chalk.gray('\n    at ')
-      const stack = at + lines.map(toStackLine).join(at)
+      const stack = at + lines.map(toStackLines).join(at)
       if (hasAnsi(err.message)) {
         const error = chalk.red.bold('Error:')
-        console.error('🚫 ', error, err.message, stack, ...rest)
+        console.error('🚫 ', error, err.message, stack, ...rest.map(toRest))
       } else {
         console.error('🚫 ', chalk.red.bold(err.message), stack)
       }
     } else {
-      console.error('🚫 ', chalk.red.bold(err), ...rest)
+      console.error('🚫 ', chalk.red.bold(err), ...rest.map(toRest))
     }
   }
 
   success (...messages) {
     const [message, ...rest] = messages
-    console.log('✅ ', chalk.green.bold(message), ...rest)
+    console.log('✅ ', chalk.green.bold(message), ...rest.map(toRest))
   }
 
   log (...messages) {
     const [message, ...rest] = messages
-    console.log('💬 ', chalk.bold(message), ...rest)
+    console.log('💬 ', chalk.bold(message), ...rest.map(toRest))
   }
 
   warn (...messages) {
     const [message, ...rest] = messages
-    console.warn('⚠️  ', chalk.yellow.bold(message), ...rest)
+    console.warn('⚠️  ', chalk.yellow.bold(message), ...rest.map(toRest))
   }
 
   info (...messages) {
     const [message, ...rest] = messages
-    console.info('💁 ', chalk.blue.bold(message), ...rest)
+    console.info('💁 ', chalk.blue.bold(message), ...rest.map(toRest))
   }
 
   debug (...messages) {
     const [message, ...rest] = messages
-    console.debug('🐛 ', chalk.magenta.bold(message), ...rest)
+    console.debug('🐛 ', chalk.magenta.bold(message), ...rest.map(toRest))
   }
 }
 
 export const print = new Print()
+
+export { chalk }
